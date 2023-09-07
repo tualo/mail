d6916d70ecc2f1a27148f0f880ff8ae19b4cc0df
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-09-07 21:26:41 +0200

    add pass

--- src/sql/ddl.sql
+++ src/sql/ddl.sql
@@ -3,9 +3,9 @@ create table if not exists `mail_config` (
     `smtp_host` varchar(255) NOT NULL,
     `smtp_auth` varchar(255) NOT NULL,
     `smtp_user` varchar(255) NOT NULL,
+    `smtp_pass` varchar(255) NOT NULL,
     `smtp_secure` varchar(255) NOT NULL,
     `smtp_no_autotls` tinyint default 0,
     `smtp_no_certcheck`  tinyint default 0,
     PRIMARY KEY (`id`)
 );
-
